f1e5d3cc7f8d4bae5ea175600a7e46ffaa1417c1
Theo Henson <theodorehenson@protonmail.com>
2020-12-10 18:48:32 -0500

    Refactor

--- drmo.py
+++ drmo.py
@@ -1,86 +1,7 @@
 #!/usr/bin/env python3
 import cv2
 import PySimpleGUI as sg
-
-# Define constants for colors
-RED = (0, 0, 255)
-GREEN = (0, 255, 0)
-
-# TODO: Implement CNN
-def predict(eye):
-    """Returns False if the input eye image is closed. True if open."""
-
-    return True
-
-def render(frame, face_detector, open_eyes_detector, left_eye_detector, right_eye_detector, frame_x, frame_y):
-    """Returns a rendered frame of video to be displayed. Takes input from a video capture,
-    uses OpenCV classifiers and draws box around certain features."""
-
-    # Apply some filters to normalize the image
-    frame = cv2.resize(frame, (frame_x, frame_y), fx=0.6, fy=0.6) # resize
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # extract greyscale
-    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # extract rgb
-    
-    # Detect faces
-    faces = face_detector.detectMultiScale(
-        gray,
-        scaleFactor=1.2,
-        minNeighbors=5,
-        minSize=(50, 50),
-        flags=cv2.CASCADE_SCALE_IMAGE
-    )
-
-    # Iterate coordinates of each detected face
-    for (x, y, w, h) in faces:
-
-        # Extract just the face from the frame
-        face = frame[y:y+h, x:x+w]
-        gray_face = gray[y:y+h, x:x+w]
-
-        # Separate the face into left and right sides
-        left_face = frame[y:y+h, x+int(w/2):x+w]
-        left_face_gray = gray[y:y+h, x+int(w/2):x+w]
-
-        right_face = frame[y:y+h, x:x+int(w/2)]
-        right_face_gray = gray[y:y+h, x:x+int(w/2)]
-
-        # Detect the left eye
-        left_eye = left_eye_detector.detectMultiScale(
-            left_face_gray,
-            scaleFactor=1.1,
-            minNeighbors=5,
-            minSize=(30, 30),
-            flags = cv2.CASCADE_SCALE_IMAGE
-        )
-
-        # Detect the right eye
-        right_eye = right_eye_detector.detectMultiScale(
-            right_face_gray,
-            scaleFactor=1.1,
-            minNeighbors=5,
-            minSize=(30, 30),
-            flags = cv2.CASCADE_SCALE_IMAGE
-        )
-
-        # Check if each eye is closed or not and draw a rectangle of the proper color
-        for (ex, ey, ew, eh) in right_eye:
-            color = GREEN
-            pred = predict(right_face[ey:ey+eh, ex:ex+ew])
-            if not pred: color = RED
-
-            cv2.rectangle(right_face, (ex, ey), (ex+ew, ey+eh), color, 2)
-
-        for (ex, ey, ew, eh) in left_eye:
-            color = GREEN
-            pred = predict(left_face[ey:ey+eh, ex:ex+ew])
-            if not pred: color = RED
-
-            cv2.rectangle(left_face, (ex, ey), (ex+ew, ey+eh), color, 2)
-    
-        # Draw a rectangle around the face
-        cv2.rectangle(frame, (x, y), (x+w, y+h), GREEN, 2)
-
-    return frame
+from features import Eye, Face, Frame
 
 if __name__ == '__main__':
 
@@ -114,17 +35,23 @@ if __name__ == '__main__':
         resizable=True
     )
 
-
+    past = None
     while window(timeout=20)[0] != sg.WIN_CLOSED:
 
-        # Read from the video capture and render the frame
-        ret, frame = cap.read()
+        # Read from the video capture and instantiate the frame
+        ret, data = cap.read()
         if not ret: break
-        frame = render(frame, face_detector, open_eyes_detector, left_eye_detector, right_eye_detector, frame_width, frame_height)
+        if past != None: frame = Frame(data, (frame_width, frame_height), past)
+        else: frame = Frame(data, (frame_width, frame_height), [])
+
+        # Detect faces and render the rectangles on the frame
+        frame.detect(face_detector, open_eyes_detector, left_eye_detector, right_eye_detector)
+        past = frame.past
+        draw = frame.render()
 
         # Update the GUI and write to a file
-        window['image'](data=cv2.imencode('.png', frame)[1].tobytes())
-        out.write(frame)
+        window['image'](data=cv2.imencode('.png', draw)[1].tobytes())
+        out.write(draw)
 
     # Clean up the display window and write the file
     cap.release()
---
+++ features.py
@@ -0,0 +1,116 @@
+import cv2
+
+# HIST is the number of past faces to remember.
+# At 30 fps 30 faces would be one second of recording.
+HIST = 150
+RED = (0, 0, 255)
+GREEN = (0, 255, 0)
+THICKNESS = 2
+
+# TODO: Implement CNN
+def predict(eye):
+    """Returns False if the input eye image is closed. True if open."""
+
+    return True
+
+class Eye:
+    """Represents a detected eyes's state"""
+
+    def __init__(self, opened, coords):
+        self.opened = opened
+        self.coords = coords
+
+class Face:
+    """Represents a detected face's state"""
+
+    def __init__(self, eyes, coords):
+        self.eyes = eyes
+        self.past = []
+        self.coords = coords
+
+class Frame:
+    """Represents a single frame's state"""
+
+    def __init__(self, data, coords, past):
+        self.coords = coords
+        self.data = cv2.resize(data, self.coords, fx=0.6, fy=0.6)
+        self.gray = cv2.cvtColor(self.data, cv2.COLOR_BGR2GRAY)
+        self.faces = []
+        self.past = past
+
+    def render(self):
+        """Returns a frame with rendered shapes on it."""
+
+        draw = self.data.copy()
+        for face in self.faces:
+            x, y, w, h = face.coords
+            cv2.rectangle(draw, (x, y), (x+w, y+h), GREEN, THICKNESS)
+            left_face = draw[y:y+h, x+int(w/2):x+w]
+            right_face = draw[y:y+h, x:x+int(w/2)]
+
+            for i, eye in enumerate(face.eyes):
+                if eye == None: continue
+                color = GREEN if eye.opened else RED
+                side = left_face if i else right_face
+                ex, ey, ew, eh = eye.coords
+                cv2.rectangle(side, (ex, ey), (ex+ew, ey+eh), color, THICKNESS)
+
+        return draw
+
+    def detect(self, face_detector, open_eyes_detector, left_eye_detector, right_eye_detector):
+        """Detect faces and eyes within the frame."""
+
+        # Detect faces
+        faces = face_detector.detectMultiScale(
+            self.gray,
+            scaleFactor=1.2,
+            minNeighbors=5,
+            minSize=(50, 50),
+            flags=cv2.CASCADE_SCALE_IMAGE
+        )
+
+        # Iterate coordinates of each detected face
+        for (x, y, w, h) in faces:
+
+            # Extract just the face from the frame
+            face = self.data[y:y+h, x:x+w]
+            gray_face = self.gray[y:y+h, x:x+w]
+
+            # Separate the face into left and right sides
+            left_face = self.data[y:y+h, x+int(w/2):x+w]
+            left_face_gray = self.gray[y:y+h, x+int(w/2):x+w]
+            right_face = self.data[y:y+h, x:x+int(w/2)]
+            right_face_gray = self.gray[y:y+h, x:x+int(w/2)]
+
+            # Detect the left eye
+            left_eye = left_eye_detector.detectMultiScale(
+                left_face_gray,
+                scaleFactor=1.1,
+                minNeighbors=5,
+                minSize=(30, 30),
+                flags = cv2.CASCADE_SCALE_IMAGE
+            )
+
+            # Detect the right eye
+            right_eye = right_eye_detector.detectMultiScale(
+                right_face_gray,
+                scaleFactor=1.1,
+                minNeighbors=5,
+                minSize=(30, 30),
+                flags = cv2.CASCADE_SCALE_IMAGE
+            )
+
+            # Check if each eye is closed or not and draw a rectangle of the proper color
+            re, le = [], []
+            for (ex, ey, ew, eh) in right_eye:
+                re.append(Eye(predict(right_face[ey:ey+eh, ex:ex+ew]), (ex, ey, ew, eh)))
+            for (ex, ey, ew, eh) in left_eye:
+                le.append(Eye(predict(left_face[ey:ey+eh, ex:ex+ew]), (ex, ey, ew, eh)))
+
+            if len(re) < 1: re.append(None)
+            if len(le) < 1: le.append(None)
+
+            self.faces.append(Face((re[0], le[0]), (x, y, w, h)))
+
+        self.past.append(self.faces)
+        self.past = self.past[-HIST:]
